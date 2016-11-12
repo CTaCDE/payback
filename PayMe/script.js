3d079b2cedc333b11b96f616950ce2bb2ad49973
risingjason <jzhang5137@gmail.com>
2016-11-12 15:18:51 -0800

    Update script.js

--- PayMe/script.js
+++ PayMe/script.js
@@ -27,14 +27,10 @@ function toDataUrl(url, callback) {
                 try {
         /*            // Create ObjectURL
                     var imgURL = window.URL.createObjectURL(file);
-
                     // Set img src to ObjectURL
                     showPicture.src = imgURL;
-
                     // Revoke ObjectURL
                     URL.revokeObjectURL(imgURL);
-
-
                       */
                       var fileReader = new FileReader();
                       fileReader.onload = function (event) {
